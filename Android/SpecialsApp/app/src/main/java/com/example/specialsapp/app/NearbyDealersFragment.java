c31ed532442ab42466bae094dfb47d05bc040150
Austin Browne <dazztrazak@gmail.com>
2014-06-17 15:52:11 -0400

    Location on back burner - correct back funcitonality and added dealers as home page, then specials

--- Android/SpecialsApp/app/src/main/java/com/example/specialsapp/app/NearbyDealersFragment.java
+++ Android/SpecialsApp/app/src/main/java/com/example/specialsapp/app/NearbyDealersFragment.java
@@ -1,5 +1,6 @@
 package com.example.specialsapp.app;
 
+import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -7,39 +8,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
-import it.gmariotti.cardslib.library.internal.CardHeader;
 import it.gmariotti.cardslib.library.view.CardListView;
 
 /**
  * Created by brownea on 6/12/14.
  */
-public class ConsumerHomeFragment extends Fragment {
+public class NearbyDealersFragment extends Fragment {
 
     private View homeView;
     private ArrayList<Card> cards = new ArrayList<Card>();
     private CardListView cardListView;
+    private TextView dealerName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        homeView = inflater.inflate(R.layout.fragment_consumer_home, container, false);
+        homeView = inflater.inflate(R.layout.fragment_nearby_dealers_, container, false);
 
-        SpecialCard card  = new SpecialCard(getActivity());
+        ActionBar actionBar = ((HomeActivity)getActivity()).getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
 
-        for (int i = 0; i < 5; i++){
+        SpecialCard card = new SpecialCard(getActivity(), R.layout.dealer_card);
+        card.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+                DealerSpecialsFragment nearbyDealersFragment = new DealerSpecialsFragment();
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer2, nearbyDealersFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        for (int i = 0; i < 5; i++) {
             cards.add(card);
         }
 
         CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(getActivity(), cards);
 
         cardListView = (CardListView) homeView.findViewById(R.id.myList);
-        if (cardListView != null){
+        if (cardListView != null) {
             cardListView.setAdapter(mCardArrayAdapter);
         }
 
