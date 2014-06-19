88f76b2d1fa7592396d99c525b42d4a7b7e4343d
maharb <brmahar@mtu.edu>
2014-06-19 15:11:36 -0400

    Added a few minor things and nav drawer

--- Service/Specials/src/main/java/api/models/Dealer.java
+++ Service/Specials/src/main/java/api/models/Dealer.java
@@ -56,6 +56,48 @@ public class Dealer {
     private String admin;
     private Location loc;
 
+    public Dealer(String name, String admin, Location loc, String city, String state, Point location) {
+        this.name = name;
+        this.admin = admin;
+        this.loc = loc;
+        this.city = city;
+        this.state = state;
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Dealer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", admin='" + admin + '\'' +
+                ", loc=" + loc +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", location=" + location +
+                '}';
+    }
+
+    private String city;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    private String state;
+
     public Dealer(Point location, Location loc, String admin, String name) {
         this.location = location;
         this.loc = loc;
@@ -69,17 +111,6 @@ public class Dealer {
 
     public void setLocation(Point location) {
         this.location = location;
-    }
-
-    @Override
-    public String toString() {
-        return "Dealer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", admin='" + admin + '\'' +
-                ", loc=" + loc +
-                ", location=" + location +
-                '}';
     }
 
     @JsonIgnore
