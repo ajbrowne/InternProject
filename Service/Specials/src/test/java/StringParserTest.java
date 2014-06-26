a4fc905df91ed31121a214b70208965d0aabed67
maharb <brmahar@mtu.edu>
2014-06-26 15:36:56 -0400

    UI fixes and starting to write more complex search

---
+++ Service/Specials/src/test/java/StringParserTest.java
@@ -0,0 +1,14 @@
+import helpers.StringParser;
+import org.junit.Test;
+
+/**
+ * Created by maharb on 6/26/14.
+ */
+public class StringParserTest {
+
+    @Test
+    public void parserTest(){
+        String test = "2014 428 Gran Coupe BMW";
+        System.out.println(StringParser.mapValues(StringParser.parseString(test)));
+    }
+}
