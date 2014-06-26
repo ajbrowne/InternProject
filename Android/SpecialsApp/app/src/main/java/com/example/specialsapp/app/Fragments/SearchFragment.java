5b0447d23f7ad048ed9f16124afcebb4bc875ca1
Austin Browne <dazztrazak@gmail.com>
2014-06-26 09:01:16 -0400

    More search value grabbing/start of async

--- Android/SpecialsApp/app/src/main/java/com/example/specialsapp/app/Fragments/SearchFragment.java
+++ Android/SpecialsApp/app/src/main/java/com/example/specialsapp/app/Fragments/SearchFragment.java
@@ -5,14 +5,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import com.example.specialsapp.app.Models.Special;
 import com.example.specialsapp.app.R;
+import com.example.specialsapp.app.Rest.SpecialsRestClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.view.CardListView;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
 public class SearchFragment extends Fragment {
 
@@ -21,6 +38,9 @@ public class SearchFragment extends Fragment {
     private Spinner yearSpinner;
     private Spinner priceSpinner;
     private Spinner typeSpinner;
+    private Button submitSearch;
+    private String[] params = new String[5];
+    private RequestParams parameters;
 
 
     public SearchFragment() {
@@ -34,31 +54,64 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         View searchView = inflater.inflate(R.layout.fragment_search, container, false);
 
-        initializeSpinners(searchView);
-
-        return searchView;
-    }
-
-    public void initializeSpinners(View searchView){
+        submitSearch = (Button) searchView.findViewById(R.id.searchButton);
         makeSpinner = (Spinner) searchView.findViewById(R.id.makeSpinner);
         modelSpinner = (Spinner) searchView.findViewById(R.id.modelSpinner);
         yearSpinner = (Spinner) searchView.findViewById(R.id.yearSpinner);
         priceSpinner = (Spinner) searchView.findViewById(R.id.priceSpinner);
         typeSpinner = (Spinner) searchView.findViewById(R.id.typeSpinner);
 
-        populateSpinner(makeSpinner, R.array.makes);
-        populateSpinner(modelSpinner, R.array.models_Ford);
-        populateSpinner(yearSpinner, R.array.year);
-        populateSpinner(priceSpinner, R.array.priceRange);
-        populateSpinner(typeSpinner, R.array.type);
+        submitSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return searchView;
     }
 
-    public void populateSpinner(Spinner spinner, int array_id){
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), array_id, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+    public void setSpinnerListener(final Spinner spinner, final int index){
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = spinner.getSelectedItem().toString();
+                params[index] = selected;
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
 
+    public void search(){
+        HashMap<String, String> param = new HashMap<String, String>();
+        param.put("lng", longg);
+        param.put("lat", latt);
+        parameters = new RequestParams(param);
 
+        SpecialsRestClient.get("special", parameters, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray request) {
+                ArrayList<Special> specials = new ArrayList<Special>();
+                try {
+                    JSONObject dealer = (JSONObject) request.get(0);
+                    JSONArray specialArray = (JSONArray) dealer.get("specials");
+                    for (int i = 0; i < specialArray.length(); i++) {
+                        Special special = new Special();
+                        JSONObject spec = (JSONObject) specialArray.get(i);
+                        special.setTitle(spec.getString("title"));
+                        special.setDealer(dealer.getString("dealerName"));
+                        special.setDescription(spec.getString("description"));
+                        special.setType(spec.getString("type"));
+                        specials.add(special);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+    }
 }
