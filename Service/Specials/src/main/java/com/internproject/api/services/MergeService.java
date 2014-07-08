9b269f498808c203e0528d5a602007c285e4f870
Austin Browne <dazztrazak@gmail.com>
2014-07-08 09:22:27 -0400

    search working

--- Service/Specials/src/main/java/com/internproject/api/services/MergeService.java
+++ Service/Specials/src/main/java/com/internproject/api/services/MergeService.java
@@ -110,16 +110,16 @@ public class MergeService {
      * @return a modified list of specials
      */
     private List<Special> specialHelper(List<Special> specials, List<String> vehicleIds) {
-
+        List<Special> matches = new ArrayList<Special>();
         for (int i = 0; i < specials.size(); i++) {
             for (String vehicleId : vehicleIds) {
-                if (!listCheck(specials.get(i).getVehicleId(), vehicleId)) {
-                    specials.remove(i);
+                if (listCheck(specials.get(i).getVehicleId(), vehicleId)) {
+                    matches.add(specials.get(i));
                 }
             }
         }
 
-        return specials;
+        return matches;
     }
 
     public List<Vehicle> vehicleHelper(List<Vehicle> vehicles, Vehicle match) {
@@ -143,6 +143,7 @@ public class MergeService {
 
         for (String temp : ids) {
             if (temp.equals(id)) {
+                System.out.println("TEMP: " + temp + " ID: " + id);
                 return true;
             }
         }
