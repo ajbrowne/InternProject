9be8edc5a2e1655fa20f80ae9f283afc5e2ceef4
maharb <brmahar@mtu.edu>
2014-07-08 14:22:16 -0400

    scripts

--- specials.js
+++ specials.js
@@ -6,96 +6,69 @@ db.special.insert({
   dealer : "53ad64abee4afe7993802ea1",
   amount :"6000",
   status:1,
-  vehicleId:[""],
+  vehicleId:["53bc30ab4be9fb81694b47ff"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "Cadillac Sale",
   dealer : "53ad64abee4afe7993802ea1",
-  amount :"",
+  amount :"700",
   status:1,
-  vehicleId:[""],
+  vehicleId:["53bc30ab4be9fb81694b47f5", "53bc30ab4be9fb81694b47f6", "53bc30ab4be9fb81694b47f7"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "Green Sale",
   dealer : "53ad64abee4afe7993802ea1",
-  amount :"20000",
+  amount :"2000",
   status:1,
-  vehicleId:[""],
+  vehicleId:["53bc30ab4be9fb81694b47f8"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "Family Deal",
   dealer : "53ad64abee4afe7993802ea1",
-  amount :"50",
+  amount :"650",
   status:1,
-  vehicleId:["", ""],
+  vehicleId:["53bc30ab4be9fb81694b47f7", "53bc30ab4be9fb81694b47f9", "53bc30ab4be9fb81694b47fc", "53bc30ab4be9fb81694b47fd"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "First Car Special",
   dealer : "53ad64abee4afe7993802ea1",
-  amount :"2250",
+  amount :"500",
   status:1,
-  vehicleId:[""],
+  vehicleId:["53bc30ab4be9fb81694b4804", "53bc30ab4be9fb81694b47fb", "53bc30ab4be9fb81694b4803"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "Chevy Summer Sale",
   dealer : "53ad64abee4afe7993802ea1",
-  amount :"30",
+  amount :"1000",
   status:1,
-  vehicleId:[""],
+  vehicleId:["53bc30ab4be9fb81694b4804", "53bc30ab4be9fb81694b4801", "53bc30ab4be9fb81694b4800", "53bc30ab4be9fb81694b47ff", "53bc30ab4be9fb81694b47fe"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "Buick Flash Sale",
   dealer : "53ad64abee4afe7993802ea1",
-  amount :"5000",
+  amount :"1500",
   status:1,
-  vehicleId:["",""],
+  vehicleId:["53bc30ab4be9fb81694b4802","53bc30ab4be9fb81694b4803"],
   trending :false
 });
 
 db.special.insert({
-  title: "",
+  title: "Truck Sale",
   dealer : "53ad64abee4afe7993802ea1",
   amount :"637",
   status:1,
-  vehicleId:[""],
-  trending :false
-});
-
-db.special.insert({
-  title: "",
-  dealer : "53ad64abee4afe7993802ea1",
-  amount :"6000",
-  status:1,
-  vehicleId:[""],
-  trending :false
-});
-
-db.special.insert({
-  title: "",
-  dealer : "53ad64abee4afe7993802ea1",
-  amount :"30000",
-  status:1,
-  vehicleId:["", "", "", ""],
-  trending :false
-});
-
-db.special.insert({
-  title: "",
-  dealer : "53ad64abee4afe7993802ea1",
-  amount :"999",
-  status:1,
-  vehicleId:[""],
+  vehicleId:["53bc30ab4be9fb81694b47fa", "53bc30ab4be9fb81694b4800"],
   trending :false
 });
